9d394f1f0c5d8380399de17aeb09bbcb3afb8546
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-09-04 13:56:19 -0300

    Removido códigos de exemplo e iniciado projeto.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form-capture-customer-data/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormCaptureCustomerData from './components/FormCaptureCustomerData.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormCaptureCustomerData
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/FormCaptureCustomerData.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-steps>
+        <b-step-item label="Localização" icon="map-marker"></b-step-item>
+        <b-step-item label="Profissão" icon="briefcase"></b-step-item>
+        <b-step-item label="Entidade" icon="user"></b-step-item>
+    </b-steps>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormCaptureCustomerData',
+}
+</script>
