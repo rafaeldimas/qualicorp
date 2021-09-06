59d7798766c656090735611fd6a6d19a22bc5805
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-09-06 14:37:53 -0300

    Criado o Component StepProfission.

---
+++ src/components/CaptureCustomerData/StepProfission.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-step-item label="Profissão" icon="briefcase"></b-step-item>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'StepProfisssion'
+}
+</script>
+
+<style>
+
+</style>
