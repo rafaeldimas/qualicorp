68797a3dc682d6ce617e001352fdaede7ca14bf0
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-09-06 14:37:33 -0300

    Criado o componnete StepEntity.

---
+++ src/components/CaptureCustomerData/StepEntity.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-step-item label="Entidade" icon="domain"></b-step-item>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'StepEntity'
+}
+</script>
+
+<style>
+
+</style>
