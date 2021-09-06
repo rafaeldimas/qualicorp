c66500f8814fc9ca7c47589e44500a13b67386b4
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-09-06 14:37:17 -0300

    Criado e configurado o componente StepLocation.

---
+++ src/components/CaptureCustomerData/StepLocation.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <b-step-item label="Localização" icon="map-marker">
+      <b-field label="Estado">
+        <b-select
+          placeholder="Selecione o Estado"
+          v-model="selectedState"
+          expanded
+          :loading="loadingStates"
+          :disabled="loadingStates"
+          @input="getCities"
+        >
+          <option v-for="state of states" :key="state.id" :value="state.id">
+            {{ state.nome }}
+          </option>
+        </b-select>
+      </b-field>
+
+      <b-field label="Cidade">
+        <b-select
+          placeholder="Selecione a Cidade"
+          v-model="selectedCity"
+          expanded
+          :loading="loadingCities"
+          :disabled="!cities.lenght || loadingCities"
+        >
+          <option v-for="city of cities" :key="city.id" :value="city.id">
+            {{ city.nome }}
+          </option>
+        </b-select>
+      </b-field>
+    </b-step-item>
+  </div>
+</template>
+
+<script>
+import * as locale from "@/services/api/locale";
+
+export default {
+  name: "StepLocation",
+  data() {
+    return {
+      states: [],
+      loadingStates: false,
+      selectedState: null,
+      cities: [],
+      loadingCities: false,
+      selectedCity: null
+    };
+  },
+  mounted() {
+    this.getStates();
+  },
+  methods: {
+    async getStates() {
+      this.loadingStates = true;
+      try {
+        const response = await locale.states({ orderBy: "nome" });
+        this.states = response.data;
+      } catch (e) {
+        this.$buefy.toast.open({
+          message:
+            "Não foi possivel listar os estados, tente novamente mais tarde!",
+          type: "is-error"
+        });
+      }
+      this.loadingStates = false;
+    },
+
+    async getCities() {
+      this.loadingCities = true;
+
+      try {
+        const response = await locale.cities(this.selectedState, {
+          orderBy: "nome"
+        });
+        this.cities = response.data;
+      } catch (e) {
+        this.$buefy.toast.open({
+          message:
+            "Não foi possivel listar as cidades, tente novamente mais tarde!",
+          type: "is-error"
+        });
+      }
+      this.loadingCities = false;
+    }
+  }
+};
+</script>
